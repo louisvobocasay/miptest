a212c39f5088326ddc1678a34e41dbaaa720743f
Louis <svohong@bocasay.com>
2020-06-26 11:09:45 +0700

    Intialized comment component and configured its routing

--- src/containers/posts/posts-routing.module.ts
+++ src/containers/posts/posts-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PostsComponent } from './posts.component';
+import { CommentsComponent } from './comments/comments.component';
 
 
 const routes: Routes = [
@@ -8,6 +9,7 @@ const routes: Routes = [
     path: '',
     component: PostsComponent
   },
+  { path: ':id/comment', component: CommentsComponent }
 ];
 
 @NgModule({
--- src/containers/posts/posts.module.ts
+++ src/containers/posts/posts.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostsComponent } from './posts.component';
 import { PostsRoutingModule } from './posts-routing.module';
+import { CommentsComponent } from './comments/comments.component';
 
 
 @NgModule({
-  declarations: [PostsComponent],
+  declarations: [PostsComponent, CommentsComponent],
   imports: [
     CommonModule,
     PostsRoutingModule,
